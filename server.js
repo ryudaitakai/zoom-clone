f5f25491b9e17b34903f3fd7b01eccbc76e44e43
高井 <56185180+ryudaitakai@users.noreply.github.com>
2020-11-13 15:35:41 +0900

    Initial commit
    ビデオチャットです。

---
+++ server.js
@@ -0,0 +1,35 @@
+// サーバ側の処理
+
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4 : uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public')) //publicフォルダ内のファイルを読み込めるようにする
+
+app.get('/', (req, res) => {
+    res.redirect((`/${uuidV4()}`))
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+/*connection(webSocket確立時)イベント時
+クライアントが接続するたびにイベントハンドラを登録*/
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)  // roomIdルームに接続
+        // 特定クライアントに送信
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+
+        socket.on('desconnect', () => {
+            socket.to(roomId).broadcast.emit('user-desconnected', userId)
+        })
+    })
+})
+
+
+server.listen(3000)
